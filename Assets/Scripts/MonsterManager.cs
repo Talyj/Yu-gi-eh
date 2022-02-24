8cf32c69eb19b82ad86e0ffecd63266e7d01c45a
sepiiroth <37743946+sepiiroth@users.noreply.github.com>
2022-02-24 15:05:37 +0100

    monstres + animations
    - ajout du healer
    - ajout du attacker
    - ajout du defender
    - ajout de toute les animations

---
+++ Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class MonsterManager : MonoBehaviour
+{
+    public float life;
+    public float shield;
+    public string type;
+    public bool hasShield;
+    public Animator anim;
+
+    void Update() {
+        //Pour les test d'animations, à retirer
+        if(Input.GetKeyDown("s")) {
+            OnSpawn();
+        }
+        if(Input.GetKeyDown("m")) {
+            Die();
+        }
+        if(Input.GetKeyDown("p")) {
+            GetDamage();
+        }
+        if(Input.GetKeyDown("a")) {
+            Attack(1, null);
+        }
+
+        //sauf lui
+
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("death") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1) {
+            DestroyEntity();
+        }
+    }
+
+    private void OnSpawn() {
+        anim.SetBool("Spawn", true);
+        Invoke("Reset", 0.2f);
+    }
+
+    private void Reset() {
+        anim.SetBool("GetDamage", false);
+        anim.SetBool("Attack", false);
+        anim.SetBool("Spawn", false);
+    }
+
+    public void Attack(float dmg, MonsterManager target) {
+        anim.SetBool("Attack", true);
+        Invoke("Reset", 0.2f);
+        target.GetDamage();
+        if(target.type == "defender") {
+            
+        }
+        if(this.type == "healer") {
+
+        }
+    }
+ 
+    public void GetDamage() {
+        anim.SetBool("GetDamage", true);
+        Invoke("Reset", 0.2f);
+    }
+
+    public void Die() {
+        anim.SetBool("Die", true);
+    }
+
+    private void DestroyEntity() {
+        Destroy(this.gameObject);
+    }
+
+}
